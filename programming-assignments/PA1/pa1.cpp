8439cf17b6acfa8d32612d0f4213e2884a7599e0
Tay Yang Shun <tay.yang.shun@gmail.com>
2015-02-02 02:29:09 +0800

    [PA1] Finally passed Question A

--- programming-assignments/PA1/pa1.cpp
+++ programming-assignments/PA1/pa1.cpp
@@ -16,11 +16,24 @@ using namespace std;
 
 const string digits = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 const char RADIX_POINT = '.';
-const int MAX_LEN = 10010;
 
-int lenV;
-int lenM;
-int lenTotal;
+// Debugging functions
+void printStrArray(char arr[]) {
+    int len = strlen(arr);
+    for (int i = 0; i < len; i++) {
+        cout << arr[i];
+    }
+    cout << endl;
+};
+
+void printIntArray(int arr[], int len) {
+    for (int i = len-1; i >= 0; i--) {
+        cout << arr[i];
+    }
+    cout << endl;
+};
+
+// For conversion between integer and char (base context)
 
 int valueOf(char x) { // integer value of a digit
     if ('0' <= x and x <= '9') {
@@ -34,14 +47,21 @@ int valueOf(char x) { // integer value of a digit
     return 0;
 };
 
-char digitOf(int val) { // val must be < base <= 36
+char digitOf(int val) { 
+    // val must be < base <= 36
     return digits[val];
 };
 
-//Trim leading and trailing zeros, and radixpoint
+// Helper functions
+
+void resetArray(int* arr, int len) {
+    memset(arr, 0, sizeof(int) * len);
+}
+
 string trim(string aStr) {
+    // Trim leading and trailing zeros, and radixpoint
     string X = aStr;
-    //leading zeros: 000.001
+    // leading zeros: 000.001
     while (X.length() > 1 and X[0] == '0' and X[1] != RADIX_POINT) {
         X.erase(0,1);
     }
@@ -69,9 +89,9 @@ string trim(string aStr) {
     return X;
 };
 
-void resetArray(int* arr, int len) {
-    memset(arr, 0, sizeof(int) * len);
-}
+void reverseStr(string &str) {
+    std::reverse(str.begin(), str.end());
+};
 
 void convertStrToIntArr(string str, int* arr, int len) {
     string::iterator it;
@@ -81,36 +101,7 @@ void convertStrToIntArr(string str, int* arr, int len) {
     }
 };
 
-string convertIntArrToStr(int arr[], int len) {
-    char *tmp = new char[len];
-    for (int i = 0; i < len; i++) {
-        tmp[i] = digitOf(arr[i]);
-    }
-    string S(tmp);
-    return S;
-};
-
-void reverseStr(string &str) {
-    std::reverse(str.begin(), str.end());
-};
-
-void printStrArray(char arr[]) {
-    int len = strlen(arr);
-    for (int i = 0; i < len; i++) {
-        cout << arr[i];
-    }
-    cout << endl;
-};
-
-void printIntArray(int arr[], int len) {
-    for (int i = 0; i < len; i++) {
-        cout << arr[i];
-    }
-    cout << endl;
-};
-
 void printArrayAsNumber(int arr[], int len) {
-
     string result = "";
     for (int i = len-1; i >= 0; i--) {
         result.append(1, digitOf(arr[i]));
@@ -119,18 +110,15 @@ void printArrayAsNumber(int arr[], int len) {
     cout << trim(result) << endl;
 }
 
-void intToString(int number, int base, int arr[]) {
-    arr[0] = digitOf(number % base);
-    arr[1] = digitOf(number / base);
-};
-
-void multiplyArrays(int aArray[], int bArray[], int lenA, int lenB, int base, int rArray[]) {
+void multiplyArrays(int aArray[], int bArray[], 
+                    int lenA, int lenB, 
+                    int base, int rArray[]) {
     int lenTotal = lenA + lenB;
     resetArray(rArray, lenTotal);
 
-    int iArray[lenTotal];
-    int *tempArray = new int[lenTotal];
+    int *iArray = new int[lenTotal];
     int carryOver = 0;
+
     for (int i = 0; i < lenA; i++) {
         carryOver = 0;
         resetArray(iArray, lenTotal);
@@ -144,21 +132,14 @@ void multiplyArrays(int aArray[], int bArray[], int lenA, int lenB, int base, in
 
         // Add iArray to existing rArray
         carryOver = 0;
-        for (int k = i; k <= j+i; k++) {
-            int product = rArray[k] + iArray[k] + carryOver;
-            rArray[k] = product % base;
-            carryOver = product / base;
+        j = 0;
+        for (j = 0; j <= lenB; j++) {
+            int sum = rArray[j+i] + iArray[j+i] + carryOver;
+            rArray[j+i] = sum % base;
+            carryOver = sum / base;
         }
-        // cout << j+i << endl;
-        rArray[j+i+1] = carryOver;
-        // cout << lenA << "," << lenB << "," << j+i << endl;
     }
 }
-
-long int milisecond(struct timeval tp){
-    long int ms = tp.tv_sec * 1000 + tp.tv_usec / 1000;
-    return ms;
-};
 
 int main() {
     int T;
@@ -172,13 +153,13 @@ int main() {
         reverseStr(V);
         reverseStr(M);
         
-        lenV = V.length();
-        lenM = M.length();
-        lenTotal = lenV + lenM;
+        int lenV = V.length();
+        int lenM = M.length();
+        int lenTotal = lenV + lenM;
 
         int vArray[lenV];
         int mArray[lenM];
-        int *rArray = new int[lenTotal];
+        int *rArray = new int[lenTotal+1];
 
         resetArray(vArray, lenV);
         resetArray(mArray, lenM);
@@ -186,11 +167,9 @@ int main() {
 
         convertStrToIntArr(V, vArray, lenV);
         convertStrToIntArr(M, mArray, lenM);
-        // cout << lenV << lenM << lenTotal << endl;
         multiplyArrays(vArray, mArray, lenV, lenM, base, rArray);
-        // printIntArray(rArray, lenTotal);
         printArrayAsNumber(rArray, lenTotal);
-    };
+    }; 
 
     return 0;
 }
